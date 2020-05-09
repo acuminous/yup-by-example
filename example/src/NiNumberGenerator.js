315cdb5e9793f0b7da3929ce0f7f83735c16917d
Stephen Cresswell <git@stephen-cresswell.net>
2020-05-09 12:53:29 +0100

    Improve ni-number generator

--- example/src/NiNumberGenerator.js
+++ example/src/NiNumberGenerator.js
@@ -3,9 +3,10 @@ const { BaseGenerator } = require('../../lib/');
 class NiNumberGenerator extends BaseGenerator {
 
   generate(schema, value, originalValue) {
-    const start = this.chance.string({ length: 2 });
+    const pool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const start = this.chance.string({ length: 2, pool });
     const middle = this.chance.integer({ min: 100000, max: 999999 });
-    const end = this.chance.string({ length: 1 });
+    const end = this.chance.string({ length: 1, pool });
     return `${start}${middle}${end}`.toUpperCase();
   }
 }
