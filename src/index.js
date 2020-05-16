d333a3225fa1a4e95dc74a971b9899520ef7861a
Stephen Cresswell <git@stephen-cresswell.net>
2020-05-16 18:01:31 +0100

    Export TestDataSession

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const TestDataFactory = require('./TestDataFactory');
+const TestDataSession = require('./TestDataSession');
 const BaseGenerator = require('./generators/BaseGenerator');
 const yupByExample = require('./yup-by-example');
 
 yupByExample.TestDataFactory = TestDataFactory;
+yupByExample.TestDataSession = TestDataSession;
 yupByExample.BaseGenerator = BaseGenerator;
 
 module.exports = yupByExample;
